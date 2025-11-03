e8fff591b4eb937a475742f5f692afdcc6f42ce5
Darren Vong <darren@conduktor.io>
2025-11-03 19:07:24 +0000

    Add Storybook 10 support

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entry: ["src/index.ts", "src/preview.ts", "src/manager.ts"],
+  entry: ["src/index.ts", "src/preview.ts", "src/manager.ts", "src/preset.ts"],
   splitting: false,
   minify: !options.watch,
-  format: ["cjs", "esm"],
+  format: ["esm"],
   dts: {
     resolve: true,
   },
@@ -12,7 +12,4 @@ export default defineConfig((options) => ({
   sourcemap: true,
   clean: true,
   platform: "browser",
-  esbuildOptions(options) {
-    options.conditions = ["module"];
-  },
 }));
